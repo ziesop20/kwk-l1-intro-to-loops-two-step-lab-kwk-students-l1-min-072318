7086ad20eb0bfa8dfc2c49d5ae12dc7b4890567f
ziesop20 <sincerelysophie@hotmail.com>
2018-07-24 20:34:28 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -34,7 +34,21 @@
 # end
 # a_few_more_steps
 
-def how_many_steps?
+# def how_many_steps?
+#   steps=0 
+#   loop do
+#   steps += 1 
+#   puts steps 
+#   if steps % 2 ==0 
+#     puts "left"
+#   else 
+#     puts "Right"
+#   end 
+#   sleep (0.5)
+# end 
+# end 
+
+def break_dance
   steps=0 
   loop do
   steps += 1 
@@ -45,12 +59,7 @@ def how_many_steps?
     puts "Right"
   end 
   sleep (0.5)
-end 
-end 
-
-def break_dance
-  # Write a solution that uses the same code as how_many_steps?, but breaks the
-  # loop if steps is equal to 6
+  if steps== 6
+    break
 end
 
-how_many_steps? 
