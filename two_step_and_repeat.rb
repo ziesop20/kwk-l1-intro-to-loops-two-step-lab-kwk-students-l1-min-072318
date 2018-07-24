701733090753e7e59023608e56d1e01032cc75df
ziesop20 <sincerelysophie@hotmail.com>
2018-07-24 20:33:12 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,17 +1,56 @@
-def first_steps
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
-end
+# def first_steps
+#   loop do
+#   puts "Right foot back"
+#   sleep (0.5)
+#   puts "Left foot back"
+#   sleep (0.5)
+#   puts "Right foot back"
+#   sleep (0.5)
+#   puts "Stop"
+#   sleep(1)
+# end
+# end
+# first_steps 
 
-def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
-end
+# def a_few_more_steps
+#   loop do
+#   puts "Right foot back"
+#   sleep (0.5)
+#   puts "Left foot back"
+#   sleep (0.5)
+#   puts "Right foot back"
+#   sleep (0.5)
+#   puts "Stop"
+#   sleep(1)
+#   puts "right foot steps right and back"
+#   sleep (0.5)
+#   puts "Left foot crosses over right"
+#   sleep (0.5)
+#   puts "Right foot steps right"
+#   sleep (0.5)
+#   puts "Turn"
+#   sleep (1)
+# end
+# end
+# a_few_more_steps
 
 def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
-end
+  steps=0 
+  loop do
+  steps += 1 
+  puts steps 
+  if steps % 2 ==0 
+    puts "left"
+  else 
+    puts "Right"
+  end 
+  sleep (0.5)
+end 
+end 
 
 def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
 end
+
+how_many_steps? 
